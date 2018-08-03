4046b5cef2a960a5c609dadee60f408013d1d333
Albert Hopkins <marduk@letterboxes.org>
2018-08-02 22:03:16 -0700

    circle: refactor inside() and intersects()
    These methods compute the distance between two points.  The Point class
    already have a .distance() method.  Let's use that!

--- circle.py
+++ circle.py
@@ -27,25 +27,13 @@ class Circle:
 
     def intersects(self, other: "Circle") -> bool:
         """Return True iff the other circle intersects"""
-        x1 = self.center.x
-        y1 = self.center.y
-        r1 = self.radius
-        x2 = other.center.x
-        y2 = other.center.y
-        r2 = other.radius
 
-        return (x1 - x2)**2 + (y1 - y2)**2 <= (r1 + r2)**2
+        return self.center.distance(other.center) <= self.radius + other.radius
 
     def inside(self, point: Point) -> bool:
         """Return true iff the point is inside the circle"""
-        x1 = self.center.x
-        y1 = self.center.y
-        r = self.radius
-        x2 = point.x
-        y2 = point.y
 
-        # I copied this from Google
-        return (x1 - x2)**2 + (y1 - y2)**2 < r**2
+        return self.center.distance(point) < self.radius
 
     def concentric(self, other: "Circle") -> bool:
         """Return True iff the circles are concentric"""
